22881c7cc1e42edef25dc7b079e4d70fc32bd798
Tommy NgX <58539340+Tommy-Ngx@users.noreply.github.com>
2022-11-06 12:46:04 +1100

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# Necessary packages
+import os, cv2
+import numpy as np
+import pandas as pd
+import random, tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+class BuildingsDataset(torch.utils.data.Dataset):
+
+    """Massachusetts Buildings Dataset. Read images, apply augmentation and preprocessing transformations.
+    
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_rgb_values (list): RGB values of select classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline 
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing 
+            (e.g. noralization, shape manipulation, etc.)
+    
+    """
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            class_rgb_values=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        
+        self.image_paths = [os.path.join(images_dir, image_id) for image_id in sorted(os.listdir(images_dir))]
+        self.mask_paths = [os.path.join(masks_dir, image_id) for image_id in sorted(os.listdir(masks_dir))]
+
+        self.class_rgb_values = class_rgb_values
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        
+        # read images and masks
+        image = cv2.cvtColor(cv2.imread(self.image_paths[i]), cv2.COLOR_BGR2RGB)
+        mask = cv2.cvtColor(cv2.imread(self.mask_paths[i]), cv2.COLOR_BGR2RGB)
+        
+        # one-hot-encode the mask
+        mask = one_hot_encode(mask, self.class_rgb_values).astype('float')
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        # return length of 
+        return len(self.image_paths)
+        
+
+def get_training_augmentation():
+    train_transform = [    
+        album.RandomCrop(height=256, width=256, always_apply=True),
+        album.OneOf(
+            [
+                album.HorizontalFlip(p=1),
+                album.VerticalFlip(p=1),
+                album.RandomRotate90(p=1),
+            ],
+            p=0.75,
+        ),
+    ]
+    return album.Compose(train_transform)
+
+
+def get_validation_augmentation():   
+    # Add sufficient padding to ensure image is divisible by 32
+    test_transform = [
+        album.PadIfNeeded(min_height=1536, min_width=1536, always_apply=True, border_mode=0),
+    ]
+    return album.Compose(test_transform)
+
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+
+def get_preprocessing(preprocessing_fn=None):
+    """Construct preprocessing transform    
+    Args:
+        preprocessing_fn (callable): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    """   
+    _transform = []
+    if preprocessing_fn:
+        _transform.append(album.Lambda(image=preprocessing_fn))
+    _transform.append(album.Lambda(image=to_tensor, mask=to_tensor))
+        
+    return album.Compose(_transform)
---
+++ utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Necessary packages
+import os, cv2
+import numpy as np
+import pandas as pd
+import random, tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+# Center crop padded image / mask to original image dims
+def crop_image(image, target_image_dims=[1500,1500,3]):
+   
+    target_size = target_image_dims[0]
+    image_size = len(image)
+    padding = (image_size - target_size) // 2
+
+    return image[
+        padding:image_size - padding,
+        padding:image_size - padding,
+        :,
+    ]
+
+
+
+
+
+
+
+
